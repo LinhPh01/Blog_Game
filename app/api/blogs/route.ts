e6d9fabbe2b8713e80bbba1ffde6601fe2989397
Linh Phạm <93432484+LinhPh01@users.noreply.github.com>
2024-07-10 10:49:34 +0700

    cap nhat moi nhat10

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,15 +1,24 @@
 import { NextResponse } from "next/server";
 
-import prisma from "../../lib/prismadb";
+
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
-export async function generateStaticParams() {
-  // Đây là một ví dụ đơn giản, bạn có thể thay đổi để phù hợp với logic của bạn
-  return [
-    { params: { blogId: String } },
+import { PrismaClient } from '@prisma/client';
 
-    // Thêm các tham số cho từng blogId khác nếu cần thiết
-  ];
+const prisma = new PrismaClient();
+
+export async function generateStaticParams() {
+  try {
+    const blogs = await prisma.blog.findMany(); // Lấy danh sách các blog từ cơ sở dữ liệu
+
+    // Trả về một mảng các đối tượng, mỗi đối tượng chứa các tham số cho từng blogId cụ thể
+    return blogs.map((blog) => ({
+      params: { blogId: blog.id },
+    }));
+  } catch (error) {
+    console.error('Error fetching blogs:', error);
+    throw new Error('Failed to fetch blogs');
+  }
 }
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
