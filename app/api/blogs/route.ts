e9340d3e8e72be3dc7b1e8f46fbd7627c4ce433a
Linh Phạm <93432484+LinhPh01@users.noreply.github.com>
2024-07-10 11:03:42 +0700

    cap nhat moi nhat10

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -3,23 +3,6 @@ import { NextResponse } from "next/server";
 import prisma from "../../lib/prismadb";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
-// import { PrismaClient } from '@prisma/client';
-
-// const prisma = new PrismaClient();
-
-export async function generateStaticParams() {
-  try {
-    const blogs = await prisma.blog.findMany(); // Lấy danh sách các blog từ cơ sở dữ liệu
-
-    // Trả về một mảng các đối tượng, mỗi đối tượng chứa các tham số cho từng blogId cụ thể
-    return blogs.map((blog) => ({
-      params: { blogId: blog.id },
-    }));
-  } catch (error) {
-    console.error("Error fetching blogs:", error);
-    throw new Error("Failed to fetch blogs");
-  }
-}
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
 
--- app/blogs/[blogId]/page.tsx
+++ app/blogs/[blogId]/page.tsx
@@ -1,24 +1,9 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getBlogById from "@/app/actions/getBlogById";
 import BlogId from "@/components/blog/BlogId";
-import prisma from "../../lib/prismadb";
 
 interface IParams {
   blogId: string;
-}
-
-export async function generateStaticParams() {
-  try {
-    const blogs = await prisma.blog.findMany(); // Lấy danh sách các blog từ cơ sở dữ liệu
-
-    // Trả về một mảng các đối tượng, mỗi đối tượng chứa các tham số cho từng blogId cụ thể
-    return blogs.map((blog) => ({
-      params: { blogId: blog.id },
-    }));
-  } catch (error) {
-    console.error("Error fetching blogs:", error);
-    throw new Error("Failed to fetch blogs");
-  }
 }
 
 export default async function page({ params }: { params: IParams }) {
