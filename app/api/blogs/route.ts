4f6aa6702230ce6826f8e46fe17a08eab57b0393
Linh Phạm <93432484+LinhPh01@users.noreply.github.com>
2024-07-10 10:37:59 +0700

    cap nhat moi nhat9

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -3,6 +3,14 @@ import { NextResponse } from "next/server";
 import prisma from "../../lib/prismadb";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
+export async function generateStaticParams() {
+  // Đây là một ví dụ đơn giản, bạn có thể thay đổi để phù hợp với logic của bạn
+  return [
+    { params: { blogId: String } },
+
+    // Thêm các tham số cho từng blogId khác nếu cần thiết
+  ];
+}
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
 
