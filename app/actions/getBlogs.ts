ed95dc3aa15b3bb515aec5efcea1fee6ff3609c5
Linh Phạm <93432484+LinhPh01@users.noreply.github.com>
2024-07-12 15:15:24 +0700

    update new push

--- app/actions/getBlogs.ts
+++ app/actions/getBlogs.ts
@@ -7,21 +7,17 @@ import prisma from "../lib/prismadb";
 // }
 
 export default async function getBlogs() {
-  try {
-    const blogs = await prisma.blog.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
+  const blogs = await prisma.blog.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
-    const SafeBlogs = blogs.map((blog) => ({
-      // @ts-ignore
-      ...blog,
-      createdAt: blog.createdAt.toISOString(),
-    }));
+  const SafeBlogs = blogs.map((blog) => ({
+    // @ts-ignore
+    ...blog,
+    createdAt: blog.createdAt.toISOString(),
+  }));
 
-    return SafeBlogs;
-  } catch (err: any) {
-    throw new Error(err);
-  }
+  return SafeBlogs;
 }
