e1c94bd254573109efd82e5ff82d397d6e0ffbba
Linh Phạm <93432484+LinhPh01@users.noreply.github.com>
2024-07-12 15:17:09 +0700

    update new push

--- app/blogsingle/page.tsx
+++ app/blogsingle/page.tsx
@@ -1,14 +1,13 @@
 import getCurrentUser from "../actions/getCurrentUser";
 import getBlogs from "../actions/getBlogs";
 import SingleBlog from "@/components/blog/SingleBlog";
-import { SafeBlogs } from "@/types";
 export default async function BlogPage() {
   const currentUser = await getCurrentUser();
   const blogs = await getBlogs();
   return (
     <div>
-      {blogs.map((item: SafeBlogs) => (
-        <SingleBlog data={item} key={item.id} currentUser={currentUser} />
+      {blogs.map((item: any, index: any) => (
+        <SingleBlog data={item} key={index} currentUser={currentUser} />
       ))}
     </div>
   );
